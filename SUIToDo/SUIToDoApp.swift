b84685f76343455c2cba10c8c9a5f9688fbae186
Fernando Martin Ortiz <ortizfernandomartin@gmail.com>
2020-10-11 23:13:14 -0300

    Initial Commit

---
+++ SUIToDo/SUIToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIToDoApp.swift
+//  SUIToDo
+//
+//  Created by Fernando Martín Ortiz on 11/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SUIToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
